47b7f25e486c53e3c8220aacc4e5d774ca21784c
encaladach <119975587+encaladach@users.noreply.github.com>
2023-07-24 04:28:51 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Home =() => {
+	const[ inputValue, setInputValue ] = useState("");
+	const[ todos, setTodos ] = useState([]);
 
-//create your first component
-const Home = () => {
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todos</h1>
+			<ul>
+				<li>
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress={(e) =>  {
+							if (e.key === "Enter"){
+								setTodos(todos.concat(inputValue));
+								setInputValue("");
+							}
+					}}
+					placeholder="What do you need to do?"></input>
+				</li>
+				{todos.map((t) => (
+					<li>
+						{t} <i class="fas fa-trash-alt"></i>
+					</li>
+				))}				
+			</ul>
+			<div>13 tasks</div>
 		</div>
 	);
 };
